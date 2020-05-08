612289424ae02114e3ed73c1fb020ec21440f15f
stack-user <stack.development.user@gmail.com>
2020-05-08 09:03:06 +0530

    updatedcode

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const fs  = require('fs');
+const options = {
+  key: fs.readFileSync("key.pem"),
+  cert: fs.readFileSync("cert.pem")
+};
+var app = express();
+const https = require('https').Server(options, app);
+const io = require('socket.io')(https, { 'Access-Control-Allow-Origin': '**'});
+
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.static(__dirname + '/dist/InPerson'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/dist/InPerson/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+  socket.on('subscribe', function(data) {
+    console.log('joining room', data.roomId);
+    socket.join(data.roomId);
+  })
+
+  socket.on('unsubscribe', function(data) {
+    console.log('leaving room', data.roomId);
+    socket.leave(data.roomId);
+  })
+
+  socket.on('send', function(data) {
+    io.sockets.in(data.roomId).emit('message', data);
+  });
+});
+
+https.listen(PORT, () => console.log(`https server listening at port number :${PORT}`));
+
+
+
+
