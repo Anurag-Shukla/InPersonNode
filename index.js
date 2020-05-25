548c4990e1f6684593c4662a155919ee4a70304c
stack-user <stack.development.user@gmail.com>
2020-05-25 10:21:15 +0530

    restructuring

--- index.js
+++ index.js
@@ -1,40 +1,9 @@
-const express = require('express');
-const fs  = require('fs');
-const options = {
-  key: fs.readFileSync("key.pem"),
-  cert: fs.readFileSync("cert.pem")
-};
-var app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, { 'Access-Control-Allow-Origin': '**'});
+const https = require('./src/server.js');
 
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 8000;
 
-
-app.use(express.static(__dirname + '/dist/InPerson'));
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/dist/InPerson/index.html');
+// Server
+https.listen(port, () => {
+   console.log(`Listening on: ${port}`);
 });
-
-io.sockets.on('connection', function(socket){
-  socket.on('subscribe', function(data) {
-    console.log('joining room', data.roomId);
-    socket.join(data.roomId);
-  })
-
-  socket.on('unsubscribe', function(data) {
-    console.log('leaving room', data.roomId);
-    socket.leave(data.roomId);
-  })
-
-  socket.on('send', function(data) {
-    io.sockets.in(data.roomId).emit('message', data);
-  });
-});
-
-http.listen(PORT, () => console.log(`http server listening at port number :${PORT}`));
-
-
-
 
---
+++ src/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const fs  = require('fs');
+
+const options = {
+  key: fs.readFileSync("./ssl-certs/key.pem"),
+  cert: fs.readFileSync("./ssl-certs/cert.pem")
+};
+
+var app = express();
+
+const https = require('https').Server(options, app);
+const io = require('socket.io')(https, { 'Access-Control-Allow-Origin': '**'});
+
+
+// app.use(express.static(__dirname + '/dist/InPerson'));
+
+app.get('/', function(req, res){
+  res.send("I am up and running");
+});
+
+io.sockets.on('connection', function(socket){
+  socket.on('subscribe', function(data) {
+    console.log('joining room', data.roomId);
+    socket.join(data.roomId);
+  })
+
+  socket.on('unsubscribe', function(data) {
+    console.log('leaving room', data.roomId);
+    socket.leave(data.roomId);
+  })
+
+  socket.on('send', function(data) {
+    io.sockets.in(data.roomId).emit('message', data);
+  });
+});
+
+module.exports = https;
+
+
+
+
